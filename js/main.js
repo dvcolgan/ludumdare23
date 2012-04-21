f6565b591262a80af85b11c0f91cd5bc3d5e77e2
David Colgan <dvcolgan@gmail.com>
2012-04-21 16:33:35 -0400

    I lost track of hours, but
    Gravity of aliens works!
    Added back the star field

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var MAP_SIDE_LENGTH = 5000;
 var MINIMAP_SIDE_LENGTH = 200;
 
 var ctx;
+var camera;
 
 var FPS = 1000 / 60;
 var numLoading;
@@ -20,7 +21,8 @@ var images = {
     uranus: 'images/uranus.png',
     neptune: 'images/neptune.png',
     starfield: 'images/starfield.jpg',
-    player: 'images/player.png'
+    player: 'images/player.png',
+    alien: 'images/alien.png'
 };
 
 var keys = {
@@ -88,7 +90,7 @@ function Planet(name, starting_theta, orbit_distance, mass) {
             this.x = Math.floor(Math.cos(this.theta) * this.orbit_distance);
             this.y = Math.floor(Math.sin(this.theta) * this.orbit_distance);
         },
-        draw: function(camera) {
+        draw: function() {
 
             ctx.beginPath()
             ctx.arc(-camera.x, -camera.y, this.orbit_distance, 0, 2 * Math.PI, false);
@@ -119,44 +121,44 @@ function Player() {
 
         update: function(planets) {
             // Apply gravity from all planets
-            for (var i = 0; i < planets.length; i++) {
-                var planet = planets[i];
+            //for (var i = 0; i < planets.length; i++) {
+            //    var planet = planets[i];
 
-                // All x, y coordinates are in the center of the object
-                var distX = Math.abs(this.x - planet.x);
-                var distY = Math.abs(this.y - planet.y);
+            //    // All x, y coordinates are in the center of the object
+            //    var distX = Math.abs(this.x - planet.x);
+            //    var distY = Math.abs(this.y - planet.y);
 
-                var dist = Math.sqrt((this.x - planet.x) * (this.x - planet.x) + (this.y - planet.y) * (this.y - planet.y));
+            //    var dist = Math.sqrt((this.x - planet.x) * (this.x - planet.x) + (this.y - planet.y) * (this.y - planet.y));
 
-                var force = planet.mass * 1000 / (dist * dist);
+            //    var force = planet.mass * 500 / (dist * dist);
 
-                var theta = Math.atan2(distX, distY);
+            //    var theta = Math.atan2(distX, distY);
 
-                var forceX = Math.sin(theta) * force;
-                var forceY = Math.cos(theta) * force;
+            //    var forceX = Math.sin(theta) * force;
+            //    var forceY = Math.cos(theta) * force;
 
 
-                //if (dist - this.width / 2 < planet.width / 2) {
-                //    // We are inside the planet - apply an opposite force
-                //    forceX *= -1.0;
-                //    forceY *= -1.0;
-                //    this.dx = 0;
-                //    this.dy = 0;
-                //}
-                if (dist - this.width / 2 < planet.width / 2) {
-                    // We are inside the planet - don't apply anymore force
-                    forceX = 0.0;
-                    forceY = 0.0;
-                }
-                if (this.x > planet.x) this.dx -= forceX;
-                if (this.x < planet.x) this.dx += forceX;
-                if (this.y > planet.y) this.dy -= forceY;
-                if (this.y < planet.y) this.dy += forceY;
+            //    //if (dist - this.width / 2 < planet.width / 2) {
+            //    //    // We are inside the planet - apply an opposite force
+            //    //    forceX *= -1.0;
+            //    //    forceY *= -1.0;
+            //    //    this.dx = 0;
+            //    //    this.dy = 0;
+            //    //}
+            //    if (dist - this.width / 2 < planet.width / 2) {
+            //        // We are inside the planet - don't apply anymore force
+            //        forceX = 0.0;
+            //        forceY = 0.0;
+            //    }
+            //    if (this.x > planet.x) this.dx -= forceX;
+            //    if (this.x < planet.x) this.dx += forceX;
+            //    if (this.y > planet.y) this.dy -= forceY;
+            //    if (this.y < planet.y) this.dy += forceY;
 
-            }
+            //}
 
-            this.dx += (keys.right - keys.left) * 1;
-            this.dy += (keys.down - keys.up) * 1;
+            this.dx += (keys.right - keys.left) * 5;
+            this.dy += (keys.down - keys.up) * 5;
 
 
             if (this.dx > 20) this.dx = 20;
@@ -178,29 +180,34 @@ function Player() {
             if (this.y > MAP_SIDE_LENGTH / 2 - this.height / 2) this.y = MAP_SIDE_LENGTH / 2 - this.height / 2;
 
         },
-        draw: function(camera) {
+        draw: function() {
             ctx.drawImage(this.image, (this.x - this.width / 2) - camera.x, (this.y - this.height / 2) - camera.y);
-            ctx.beginPath();
-            ctx.arc((this.x) - camera.x,
-                    (this.y) - camera.y,
-                    this.width / 2, 0, 2 * Math.PI, false);
-            ctx.fillStyle = "white";
-            ctx.fill();
+            //ctx.beginPath();
+            //ctx.arc((this.x) - camera.x,
+            //        (this.y) - camera.y,
+            //        this.width / 2, 0, 2 * Math.PI, false);
+            //ctx.fillStyle = "white";
+            //ctx.fill();
         }
     };
 }
 
-function bodyInPlanet(body, planets) {
+
+function pointInPlanet(ptX, ptY, planets) {
     var intersectingPlanet = null;
     for (var i = 0; i < planets.length; i++) {
         var planet = planets[i];
 
-        var distSq = (body.x - planet.x) * (body.x - planet.x) + (body.y - planet.y) * (body.y + planet.y);
-        if (distSq < (body.width / 2 + planet.width / 2) * (body.width / 2 + planet.width / 2)) {
+        var dist = Math.sqrt((ptX - planet.x) * (ptX - planet.x) + (ptY - planet.y) * (ptY - planet.y));
+        if (dist < planet.width / 2) {
             return planet;
         }
     }
     return null;
+}
+
+function getMagnitude(dx, dy) {
+    return Math.sqrt(dx * dx + dy * dy);
 }
 
 function Alien(startX, startY) {
@@ -211,77 +218,167 @@ function Alien(startX, startY) {
         dy: 0.0,
         image: images['alien'],
         frames: 3,
+        stall_threshold: 2.0,
+        thrust: 2.5,
+        frameTimer: 0,
+        frameLength: 5,
         curFrame: 0,
-        width: images['alien'].width,
-        height: images['alien'].height,
+        width: 50,
+        height: 40,
 
         update: function(planets) {
-            // Apply gravity from all planets
+            // Apply gravity and repulsion from all planets
             for (var i = 0; i < planets.length; i++) {
                 var planet = planets[i];
 
                 // All x, y coordinates are in the center of the object
-                var distX = Math.abs(this.x - planet.x);
-                var distY = Math.abs(this.y - planet.y);
+                var distX = this.x - planet.x;
+                var distY = this.y - planet.y;
 
                 var dist = Math.sqrt((this.x - planet.x) * (this.x - planet.x) + (this.y - planet.y) * (this.y - planet.y));
 
-                var force = planet.mass * 1000 / (dist * dist);
+                var distXNorm = distX / dist;
+                var distYNorm = distY / dist;
 
-                var theta = Math.atan2(distX, distY);
+                var forceX = 0;
+                var forceY = 0;
 
-                var forceX = Math.sin(theta) * force;
-                var forceY = Math.cos(theta) * force;
+                var evasionDist = planet.width / 2 + 200;
+                if (dist < evasionDist) {
 
+                    var distXNormPerp = -distYNorm;
+                    var distYNormPerp = distXNorm;
 
-                //if (dist - this.width / 2 < planet.width / 2) {
-                //    // We are inside the planet - apply an opposite force
-                //    forceX *= -1.0;
-                //    forceY *= -1.0;
-                //    this.dx = 0;
-                //    this.dy = 0;
-                //}
-                if (dist - this.width / 2 < planet.width / 2) {
-                    // We are inside the planet - don't apply anymore force
-                    forceX = 0.0;
-                    forceY = 0.0;
+                    var evasionPercent = ((evasionDist - dist) / evasionDist);
+
+                    forceX += distXNorm * this.thrust * evasionPercent * evasionPercent;
+                    forceY += distYNorm * this.thrust * evasionPercent * evasionPercent;
+                    // Determine which direction we are going relative to the planet
+                    if (this.dx * distXNormPerp + this.dy * distYNormPerp > 0) {
+                        forceX += distXNormPerp * this.thrust;
+                        forceY += distYNormPerp * this.thrust;
+                    }
+                    else {
+                        forceX -= distXNormPerp * this.thrust;
+                        forceY -= distYNormPerp * this.thrust;
+                    }
                 }
-                if (this.x > planet.x) this.dx -= forceX;
-                if (this.x < planet.x) this.dx += forceX;
-                if (this.y > planet.y) this.dy -= forceY;
-                if (this.y < planet.y) this.dy += forceY;
+                else {
+                    forceX += 100 * -distXNorm * planet.mass / (dist * dist);
+                    forceY += 100 * -distYNorm * planet.mass / (dist * dist);
+                }
 
+                this.dx += forceX;
+                this.dy += forceY;
             }
 
-            // TODO cast a ray out in front and the first thing it hits without going over the limit, go the other way
-            var rayX = this.x;
-            var rayY = this.y;
-            for (var i = 0; i < 10; i++) {
-                rayX += this.dx;
-                rayY += this.dy;
-
-                ctx.moveTo(this.x, this.y);
-                ctx.lineTo(rayX, rayY);
-                ctx.strokeStyle = 'red';
-                ctx.stroke();
-                
-                var planet = bodyInPlanet(this, planets);
-                if (planet) {
-                    var side = (rayX - this.x) * (planet.y - this.y) - (rayY - this.y) * (planet.x - this.x);
-                    console.log(side);
-                }
-            }
-
-            this.dx += (keys.right - keys.left) * 1;
-            this.dy += (keys.down - keys.up) * 1;
-
-            if (this.dx > 20) this.dx = 20;
-            if (this.dx < -20) this.dx = -20;
-            if (this.dy > 20) this.dy = 20;
-            if (this.dy < -20) this.dy = -20;
+            if (this.dx > 10) this.dx = 10;
+            if (this.dx < -10) this.dx = -10;
+            if (this.dy > 10) this.dy = 10;
+            if (this.dy < -10) this.dy = -10;
 
             this.x += this.dx;
             this.y += this.dy;
+
+            //console.log('closest ' + closestPlanet.name + ' ' + closestDist);
+            //if (closestDist < closestPlanet.width) {
+            //    var force = planet.mass * 500 / (dist * dist);
+
+            //    var theta = Math.atan2(distX, distY);
+            //    var forceX = Math.sin(theta) * force;
+            //    var forceY = Math.cos(theta) * force;
+
+            //    // We are inside the planet - don't apply anymore force
+            //    //if (dist - this.width / 2 < planet.width / 2) {
+            //    //    forceX = 0.0;
+            //    //    forceY = 0.0;
+            //    //}
+            //    if (this.x > closestPlanet.x) this.dx -= forceX;
+            //    if (this.x < closestPlanet.x) this.dx += forceX;
+            //    if (this.y > closestPlanet.y) this.dy -= forceY;
+            //    if (this.y < closestPlanet.y) this.dy += forceY;
+            //}
+
+            // Apply a thrust perpendicular to the closest planet if we are stalling
+
+
+            // Cast a ray out in front and the first thing it hits without going over the limit, go the other way
+            //var rayX = this.x;
+            //var rayY = this.y;
+            //for (var i = 0; i < 10; i++) {
+            //    rayX += this.dx;
+            //    rayY += this.dy;
+            //    
+            //    var planet = pointInPlanet(rayX, rayY, planets);
+            //    if (planet) {
+            //        var side = (rayX - this.x) * (planet.y - this.y) - (rayY - this.y) * (planet.x - this.x);
+            //        var sideSign = side ? side > 0 ? 1 : -1 : 0;
+
+            //        var rayLenX = rayX - this.x;
+            //        var rayLenY = rayY - this.y;
+            //        var signX = rayLenX ? rayLenX > 0 ? 1 : -1 : 0;
+            //        var signY = rayLenY ? rayLenY > 0 ? 1 : -1 : 0;
+
+            //        ctx.moveTo(this.x - camera.x, this.y - camera.y);
+            //        ctx.lineTo(rayX - camera.x, rayY - camera.y);
+            //        ctx.strokeStyle = 'green';
+            //        ctx.lineWidth = 4;
+            //        ctx.stroke();
+
+
+            //        var multX, multY;
+
+            //        if (signX > 0 && signY > 0) {
+            //            multX = -1.0;
+            //            multY = 1.0;
+            //        }
+            //        if (signX < 0 && signY > 0) {
+            //            multX = -1.0;
+            //            multY = 1.0;
+            //        }
+            //        if (signX < 0 && signY < 0) {
+            //            multX = -1.0;
+            //            multY = 1.0;
+            //        }
+            //        if (signX > 0 && signY < 0) {
+            //            multX = -1.0;
+            //            multY = 1.0;
+            //        }
+
+            //        ctx.moveTo(this.x - camera.x, this.y - camera.y);
+
+            //        var impulseX = -rayLenY * multY * sideSign;
+            //        var impulseY = -rayLenX * multX * sideSign;
+
+            //        ctx.lineTo((this.x + impulseX) - camera.x, (this.y + impulseY) - camera.y);
+            //        ctx.lineTo((this.x + -rayLenY * multY) - camera.x, (this.y + -rayLenX * multX) - camera.y);
+            //        ctx.strokeStyle = 'blue';
+            //        ctx.lineWidth = 4;
+            //        ctx.stroke();
+
+            //        //this.dx += impulseX;
+            //        //this.dy += impulseY;
+            //        this.dx += sideSign * this.thrust;
+            //        this.dy += impulseY;
+
+            //        break;
+            //    }
+            //}
+
+            // Debug line
+            //ctx.moveTo(this.x - camera.x, this.y - camera.y);
+            //ctx.lineTo(rayX - camera.x, rayY - camera.y);
+            //ctx.strokeStyle = 'red';
+            //ctx.lineWidth = 1;
+            //ctx.stroke();
+
+            // if not moving fast, apply thrust behind
+           // if (getMagnitude(this.dx, this.dy) < this.stall_threshold) {
+           //     var theta = Math.atan2(this.dx, this.dy);
+           //     this.dx += this.thrust * Math.cos(theta);
+           //     this.dy += this.thrust * Math.sin(theta);
+           // }
+
 
             // If we are going off the edge of the map, don't go further
             if (this.x < -MAP_SIDE_LENGTH / 2 + this.width / 2) this.x = -MAP_SIDE_LENGTH / 2 + this.width / 2;
@@ -289,9 +386,29 @@ function Alien(startX, startY) {
             if (this.y < -MAP_SIDE_LENGTH / 2 + this.height / 2) this.y = -MAP_SIDE_LENGTH / 2 + this.height / 2;
             if (this.y > MAP_SIDE_LENGTH / 2 - this.height / 2) this.y = MAP_SIDE_LENGTH / 2 - this.height / 2;
 
+
+            // Update sprite animations
+            this.frameTimer++;
+            if (this.frameTimer > this.frameLength) {
+                this.frameTimer = 0
+                this.curFrame++;
+                if (this.curFrame >= this.frames) {
+                    this.curFrame = 0;
+                }
+            }
+
         },
-        draw: function(camera) {
-            ctx.drawImage(this.image, (this.x - this.width / 2) - camera.x, (this.y - this.height / 2) - camera.y);
+        draw: function() {
+            ctx.drawImage(
+                this.image, 
+                this.curFrame * this.width,
+                0,
+                this.width,
+                this.height,
+                (this.x - this.width / 2) - camera.x,
+                (this.y - this.height / 2) - camera.y,
+                this.width,
+                this.height);
         }
     };
 }
@@ -318,7 +435,7 @@ function Camera() {
     };
 }
 
-function drawMinimap(camera, planets) {
+function drawMinimap(player, aliens, planets) {
     var shrinkFactor = (MAP_SIDE_LENGTH / MINIMAP_SIDE_LENGTH);
     ctx.strokeStyle = 'white';
     ctx.strokeRect(SCREEN_WIDTH - MINIMAP_SIDE_LENGTH, 
@@ -351,7 +468,7 @@ function drawMinimap(camera, planets) {
 }
 
 
-function drawStarfield(camera) {
+function drawStarfield() {
     var starfield = images['starfield'];
     var starfieldLength = starfield.width * 2;
     var scalingFactor = MINIMAP_SIDE_LENGTH / starfieldLength;
@@ -391,20 +508,23 @@ $(document).ready(function(){
             //Planet('uranus',  Math.random() * 2 * Math.PI, 1750, Math.PI * 0.001),
             //Planet('neptune', Math.random() * 2 * Math.PI, 2100, Math.PI * 0.001),
             Planet('sun', 0.0, 0, 100),
-            Planet('mercury', Math.random() * 2 * Math.PI,  300, 10),
-            Planet('venus',   Math.random() * 2 * Math.PI,  400, 20),
-            Planet('earth',   Math.random() * 2 * Math.PI,  500, 20),
-            Planet('mars',    Math.random() * 2 * Math.PI,  600, 20),
-            Planet('jupiter', Math.random() * 2 * Math.PI, 700,  70),
-            Planet('saturn',  Math.random() * 2 * Math.PI, 850,  50),
-            Planet('uranus',  Math.random() * 2 * Math.PI, 1000, 40),
-            Planet('neptune', Math.random() * 2 * Math.PI, 1150, 40)
+            Planet('mercury', Math.random() * 2 * Math.PI,  300, 100),
+            Planet('venus',   Math.random() * 2 * Math.PI,  400, 200),
+            Planet('earth',   Math.random() * 2 * Math.PI,  500, 200),
+            Planet('mars',    Math.random() * 2 * Math.PI,  600, 200),
+            Planet('jupiter', Math.random() * 2 * Math.PI, 700,  700),
+            Planet('saturn',  Math.random() * 2 * Math.PI, 850,  500),
+            Planet('uranus',  Math.random() * 2 * Math.PI, 1000, 400),
+            Planet('neptune', Math.random() * 2 * Math.PI, 1150, 400)
         ];
 
         var wave = 1;
         var aliens = [];
+        for (var i = 0; i < 100; i++) {
+            aliens.push(Alien(-1000 / 2 + Math.random() * 100, -1000 / 2 + Math.random() * 100));
+        }
 
-        var camera = Camera();
+        camera = Camera();
 
         var player = Player();
 
@@ -416,25 +536,28 @@ $(document).ready(function(){
             }
 
 
+            ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
             // Update everything
             player.update(planets);
             camera.update(player);
             for (var i = 0; i < planets.length; i++) {
                 planets[i].update();
             }
+            for (var i = 0; i < aliens.length; i++) {
+                aliens[i].update(planets);
+            }
             //alienSpawner(
 
             // Draw everything
-            ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-            drawStarfield(camera);
+            drawStarfield();
             for (var i = 0; i < planets.length; i++) {
-                planets[i].draw(camera);
+                planets[i].draw();
             }
             for (var i = 0; i < aliens.length; i++) {
-                aliens[i].draw(camera);
+                aliens[i].draw();
             }
-            player.draw(camera);
-            drawMinimap(camera, planets);
+            player.draw();
+            drawMinimap(player, aliens, planets);
 
             setTimeout(mainLoop, FPS);
         };
